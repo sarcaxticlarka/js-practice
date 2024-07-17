2137bface4f3361386fef26361e3f051a648855f
sarcaxticlarka <140724625+sarcaxticlarka@users.noreply.github.com>
2024-07-17 06:40:46 +0000

    untilconversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+let score = 33
+let scor = "33"
+
+// console.log(typeof scor)
+// console.log(score);
+
+let valueInInteger = Number(scor)
+
+let valueInString = String(score)
+
+// console.log(typeof valueInInteger);
+// console.log(typeof valueInString);
+
+// "33" => 33
+// "33abhjhdc" => Nan
+// true => 1, false => 0
+
+// let isLoggedIn = 1
+//  let booleansislogg = Boolean(isLoggedIn)
+//  console.log(booleansislogg);
+//  console.log(typeof booleansislogg);
+
+
+// let isLoggedIn = ""
+//  let booleansislogg = Boolean(isLoggedIn)
+//  console.log(booleansislogg); 
+
+let isLoggedIn = "kurkure"
+ let booleansislogg = Boolean(isLoggedIn)
+ console.log(booleansislogg); 
+
+
+// 1 => true; 0 => false
+// "" => false
+// "kurkure" => true
