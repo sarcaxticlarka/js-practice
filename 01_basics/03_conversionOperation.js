5b46e67d111a27283df0789ac047dc16c920c5b2
sarcaxticlarka <140724625+sarcaxticlarka@users.noreply.github.com>
2024-07-19 05:42:20 +0000

    until comparision

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,11 +25,43 @@ let valueInString = String(score)
 //  let booleansislogg = Boolean(isLoggedIn)
 //  console.log(booleansislogg); 
 
-let isLoggedIn = "kurkure"
- let booleansislogg = Boolean(isLoggedIn)
- console.log(booleansislogg); 
+// let isLoggedIn = "kurkure"
+//  let booleansislogg = Boolean(isLoggedIn)
+//  console.log(booleansislogg); 
 
 
 // 1 => true; 0 => false
 // "" => false
 // "kurkure" => true
+
+
+//*****************OPERATIONS*****************//
+
+// let x = 3
+// let y = -x
+// console.log(y);
+
+
+
+// basic maths operations
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// // console.log(2**2);power
+// console.log(2%2); 
+
+// let str1 = "yours"
+// let str2 = ".kurkure"
+
+// let str3 = str1+str2
+
+// console.log(str3)
+
+// console.log(1+2); //3
+// console.log("1" + 2) //12
+// console.log("1" + 2 + 3);  //123
+// console.log(1 + 2 + "3"); //33
+
+// console.log(true); //true
+// console.log(+true); //1
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,24 @@
+// conversion valu come in boolean value => true/false
+
+// console.log(2 > 1)//true
+// console.log(2 >= 1);//true
+// console.log(2<1); //false
+// console.log( 2 == 1);//false
+// console.log(2 != 1); //true
+
+// we should always try comparision same type of data type
+
+// console.log("2" > 1) 
+console.log(null >0); //false
+console.log(null == 0);//false
+console.log(null >= 0);//true
+
+console.log(undefined == 0);//false
+console.log(undefined > 0);//false
+console.log(undefined >= 0);
+console.log(undefined < 0);//false
+// likethis we not get alwayd aur pridic result becasue of diffrent data type between number otr string
+
+//=== strict check it not change only value it check dattatype also
+
+// console.log("2" === 2); //false
