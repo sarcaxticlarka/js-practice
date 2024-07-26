5263734b41e94519ea35b3fdcdf37a85d4cef734
sarcaxticlarka <140724625+sarcaxticlarka@users.noreply.github.com>
2024-07-26 04:37:05 +0000

    until whiledo loops

---
+++ 06_loops/two.js
@@ -0,0 +1,26 @@
+
+let i = 1
+
+while ( i <= 10) {
+    // console.log(` value of i is ${i}`);
+    i = i+2
+}
+
+let myArray = ["mushaf", "pir-e-kamil", "jannat ke pattey"]
+
+let arr = 0
+
+while(arr < myArray.length){
+    console.log(`fvrt book list   ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+
+
+let score = 1
+
+do {
+   
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
